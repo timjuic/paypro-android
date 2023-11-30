407adcac2686278ea5985b4265d23d6544aa39a4
matijakljaic <mkljaic20@foi.hr>
2023-11-30 22:33:24 +0100

    Designed a rought sketch of screen for adding merchants.

--- app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
+++ app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
@@ -1,10 +1,58 @@
 package com.Found404.paypro.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun AddingMerchants (
+fun AddingMerchants(
+    onCreateMerchantButtonClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround,
+    ) {
+        Text(
+            text = "PAY PRO",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(vertical = 20.dp)
+        )
+        FloatingActionButton(
+            onClick = {
+                /*TODO*/
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .size(56.dp),
+                //.align(Alignment.BottomEnd), //fix alignment
+            shape = CircleShape,
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+        }
+    }
+}
 
-){
-
+@Preview
+@Composable
+fun AddingMerchantsPreview() {
+    AddingMerchants({})
 }
