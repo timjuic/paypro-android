3678b510db3b05747e2b5775723a026879b7c81f
timjuic <tim.juic@gmail.com>
2023-12-12 11:36:15 +0100

    Add title reusable component

--- app/src/main/java/com/Found404/paypro/MainActivity.kt
+++ app/src/main/java/com/Found404/paypro/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.Found404.paypro.ui.pages.RegisterScreen
 import com.Found404.paypro.ui.theme.PayProTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
             PayProTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    RegisterScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/Found404/paypro/ui/pages/RegistrationPage.kt
@@ -0,0 +1,13 @@
+package com.Found404.paypro.ui.pages
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.Found404.paypro.ui.components.Title
+
+@Composable
+fun RegisterScreen() {
+    Title(text = "PayPro")
+}
