4d72fe2cecdf1cb4d10c7b156125a83a758532b0
timjuic <tim.juic@gmail.com>
2023-12-12 17:01:11 +0100

    Add TextInput and LabeledTextInput, update Title

--- app/src/main/java/com/Found404/paypro/ui/components/Title.kt
+++ app/src/main/java/com/Found404/paypro/ui/components/Title.kt
@@ -1,10 +1,10 @@
 package com.Found404.paypro.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -14,11 +14,13 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun Title(text: String) {
     Text(
-        text = "PayPro",
+        text = text,
         fontSize = 50.sp,
         fontWeight = FontWeight.Black,
         style = MaterialTheme.typography.titleLarge,
         textAlign = TextAlign.Center,
-        modifier = Modifier.padding(all = 20.dp)
+        modifier = Modifier
+            .padding(all = 20.dp)
+            .fillMaxWidth()
     )
 }
