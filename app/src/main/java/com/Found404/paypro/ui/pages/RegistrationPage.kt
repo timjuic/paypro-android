0255557fa6496512583ddc85ec6e034fc55d78ff
timjuic <tim.juic@gmail.com>
2023-12-13 12:19:13 +0100

    finished register page design

--- app/src/main/java/com/Found404/paypro/ui/pages/RegistrationPage.kt
+++ app/src/main/java/com/Found404/paypro/ui/pages/RegistrationPage.kt
@@ -1,12 +1,10 @@
 package com.Found404.paypro.ui.pages
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,10 +12,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.Found404.paypro.ui.components.LabeledTextInput
 import com.Found404.paypro.ui.components.PayProButton
-import com.Found404.paypro.ui.components.TextInput
 import com.Found404.paypro.ui.components.Title
 
 @Composable
@@ -39,5 +40,20 @@ fun RegisterScreen() {
         LabeledTextInput(label = "Repeat Password", value = passwordRepeat, onValueChange = { newPassword -> passwordRepeat = newPassword})
 
         PayProButton(text = "Register", onClick = { /*TODO*/ })
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            modifier = Modifier
+                .fillMaxWidth()
+            ) {
+            Text(
+                text = "Already have an account? Login",
+                color = Color.Blue,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                )
+        }
     }
 }
