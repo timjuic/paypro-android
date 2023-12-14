7b4da11071af362b873f53b8d691fe8fd957ec73
timjuic <tim.juic@gmail.com>
2023-12-14 14:28:27 +0100

    Implemented registerUser method

--- auth/src/main/java/com/Found404/paypro/RegistrationService.kt
+++ auth/src/main/java/com/Found404/paypro/RegistrationService.kt
@@ -1,3 +1,5 @@
+package com.Found404.paypro
+
 interface RegistrationService {
     suspend fun registerUser(firstName: String, lastName: String, email: String, password:String) : RegistrationResult
 
