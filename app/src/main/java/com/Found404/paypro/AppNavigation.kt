bff18accb4554eb607209ac6784c2c9d0ad1db48
matijakljaic <mkljaic20@foi.hr>
2023-12-14 18:49:47 +0100

    Removed unused code

--- app/src/main/java/com/Found404/paypro/AppNavigation.kt
+++ app/src/main/java/com/Found404/paypro/AppNavigation.kt
@@ -1,22 +1,18 @@
 package com.Found404.paypro
 
-import android.widget.Toast
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.Found404.paypro.context.MerchantAuth
 import com.Found404.paypro.ui.pages.AddingMerchants
 import com.Found404.paypro.ui.pages.CardPayments
 import com.Found404.paypro.ui.pages.MerchantAddress
 import com.Found404.paypro.ui.pages.MerchantCreated
 import com.Found404.paypro.ui.pages.MerchantName
-import com.Found404.paypro.viewModels.AddMerchantViewModel
 import com.Found404.paypro.ui.pages.RegisterScreen
 
 @Composable
-fun AppNavigation(viewModelMerchant: AddMerchantViewModel) {
+fun AppNavigation() {
 
     val navController = rememberNavController()
 
@@ -35,20 +31,9 @@ fun AppNavigation(viewModelMerchant: AddMerchantViewModel) {
                             )
                         }
                         composable("merchantName") {
-                            val context = LocalContext.current
                             MerchantName(
-                                viewModel = viewModelMerchant,
                                 onButtonNextClick = {
-                                    val merchantData = MerchantAuth.merchantData!!
-                                    if (merchantData.fullName != null) {
-                                        navController.navigate("merchantAddress")
-                                    } else {
-                                        Toast.makeText(
-                                            context,
-                                            "Please input a merchant name",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
+                                    navController.navigate("merchantAddress")
                                 },
                                 onButtonPrevClick = {
                                     navController.navigate("addingMerchants")
