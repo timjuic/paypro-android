d98043e1f11f4cffe8ba49765278209eab0a2b2d
timjuic <tim.juic@gmail.com>
2023-12-26 14:16:36 +0100

    Implement NavigationDrawer on mainpage

--- app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
+++ app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
@@ -1,73 +1,43 @@
 package com.Found404.paypro.ui.pages
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.Found404.paypro.ui.components.PayProNavigationDrawer
 
 @Composable
 fun AddingMerchants(
     navController: NavController
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding()
-            .background(color = Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround,
-    ) {
-        Text(
-            text = "PAY PRO",
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(vertical = 100  .dp)
-        )
-        Box(modifier = Modifier.fillMaxSize()){
-            FloatingActionButton(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .size(56.dp)
-                    .align(Alignment.BottomEnd),
-                onClick = {
-                    navController.navigate("merchantName")
-                },
-                shape = CircleShape,
-                containerColor = Color.Blue,
-                contentColor = Color.White
-            ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
-            }
+
+    PayProNavigationDrawer()
+
+    Box(modifier = Modifier.fillMaxSize()){
+        FloatingActionButton(
+            modifier = Modifier
+                .padding(16.dp)
+                .size(56.dp)
+                .align(Alignment.BottomEnd),
+            onClick = {
+                navController.navigate("merchantName")
+            },
+            shape = CircleShape,
+            containerColor = Color.Blue,
+            contentColor = Color.White
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
         }
     }
 }
-
-//@Preview
-//@Composable
-//fun AddingMerchantsPreview() {
-//    AddingMerchants(onCreateMerchantButtonClick = {}, onButtonCancelClick = {})
-//}
