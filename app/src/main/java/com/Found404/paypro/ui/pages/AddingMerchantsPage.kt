ff5cbdf128f851f0551c1b27c1184545d4f3585e
matijakljaic <mkljaic20@foi.hr>
2023-12-27 15:17:39 +0100

    Edited the main page so it displays mock data in a card view for displaying merchants.

--- app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
+++ app/src/main/java/com/Found404/paypro/ui/pages/AddingMerchantsPage.kt
@@ -1,19 +1,28 @@
 package com.Found404.paypro.ui.pages
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import com.Found404.paypro.ui.components.PayProNavigationDrawer
@@ -26,6 +35,47 @@ fun AddingMerchants(
     PayProNavigationDrawer(navController)
 
     Box(modifier = Modifier.fillMaxSize()){
+        Card(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .height(200.dp), // Adjust the height as needed
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Your Merchant",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.TopStart)
+                )
+                Text(
+                    text = "Konzum",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterStart)
+                )
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit",
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .size(24.dp)
+                        .clickable {
+                            // Handle click
+                        }
+                )
+                Text(
+                    text = "Click the button below to add a terminal",
+                    fontSize = 14.sp,
+                    modifier = Modifier.align(Alignment.BottomStart)
+                )
+            }
+        }
         FloatingActionButton(
             modifier = Modifier
                 .padding(16.dp)
@@ -43,3 +93,4 @@ fun AddingMerchants(
         }
     }
 }
+
