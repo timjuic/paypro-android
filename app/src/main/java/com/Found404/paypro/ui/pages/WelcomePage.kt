d93d6629713a96ca54d7f6520d03f375ddc278f6
tjuic20-foi <tim.juic@gmail.com>
2024-01-04 08:54:04 +0100

    Remove old login button

--- app/src/main/java/com/Found404/paypro/ui/pages/WelcomePage.kt
+++ app/src/main/java/com/Found404/paypro/ui/pages/WelcomePage.kt
@@ -10,27 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.Found404.paypro.AuthCallbackImpl
-import com.Found404.paypro.R
 import com.Found404.paypro.ui.components.PayProButton
 import com.Found404.paypro.ui.components.PayProHeadline
 import com.Found404.paypro.viewmodel.LoginProvidersViewModel
@@ -83,40 +72,9 @@ fun WelcomePage(navController: NavController) {
                     LayoutInflater.from(ctx).inflate(layoutId, null, false).apply {
                         findViewById<LinearLayout>(buttonId).setOnClickListener {
                             authProvider.onButtonClick(ctx, authCallback)
-//                            navController.navigate("login")
                         }
                     }
                 }
-            )
-        }
-
-        val customFontFamily = FontFamily(
-            Font(R.font.montserrat_bold, FontWeight.Bold),
-        )
-
-        val annotatedText = buildAnnotatedString {
-            withStyle(style = SpanStyle(color = Color.Black)) {
-                append("Already have an account? ")
-            }
-            withStyle(style = SpanStyle(color = Color.Blue)) {
-                append("Login")
-            }
-        }
-
-        TextButton(
-            onClick = {
-                navController.navigate("login")
-            },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth()
-                .padding(bottom = 20.dp, top = 10.dp)
-        ) {
-            Text(
-                text = annotatedText,
-                fontWeight = FontWeight.Bold,
-                fontFamily = customFontFamily,
-                fontSize = 18.sp,
             )
         }
     }
