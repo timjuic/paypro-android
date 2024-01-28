f005e04377d079edd3893b105855fb408ea6421d
dominikn1 <dnovak1@student.foi.hr>
2024-01-28 20:30:59 +0100

    Implemented Dialog instead of Surface in DeleteMerchantPopup and MessagePopup.

--- app/src/main/java/com/Found404/paypro/ui/components/MessagePopup.kt
+++ app/src/main/java/com/Found404/paypro/ui/components/MessagePopup.kt
@@ -2,56 +2,59 @@ package com.Found404.paypro.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun MessagePopup(
     message: String,
     onDismiss: () -> Unit
 ) {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Transparent), // Set the background to Transparent
-        shape = RoundedCornerShape(16.dp),
-        color = Color.Transparent
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .background(color = Color.LightGray, shape = RoundedCornerShape(16.dp))
-                .border(2.dp, Color.DarkGray, shape = RoundedCornerShape(16.dp))
-
-        ) {
-            Column(
-                modifier = Modifier.padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(dismissOnClickOutside = false),
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent),
+                contentAlignment = Alignment.Center
             ) {
-                Text(message)
-                Spacer(modifier = Modifier.height(8.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(color = Color.LightGray, shape = RoundedCornerShape(16.dp))
+                        .border(2.dp, Color.DarkGray, shape = RoundedCornerShape(16.dp)),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    PayProTitle(message, modifier = Modifier.padding(start = 16.dp))
+                    Spacer(modifier = Modifier.height(8.dp))
 
-                PayProButton(
-                    text = "OK",
-                    onClick = onDismiss
-                )
+                    PayProButton(
+                        text = "OK",
+                        onClick = onDismiss,
+                        modifier = Modifier.width(IntrinsicSize.Min)
+                    )
+                }
             }
         }
-    }
+    )
 }
